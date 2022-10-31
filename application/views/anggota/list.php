0d859c0b2c8e61ef40755aecc38039b0d0361c63
PC-14 <evolutionvalen@gmail.com>
2022-10-31 12:07:19 +0700

    fixed bug

--- application/views/anggota/list.php
+++ application/views/anggota/list.php
@@ -62,16 +62,16 @@
                         foreach ($data_anggota as $da) : ?>
 
                             <tr>
-                                <th class="align-middle text-center"><?= $no++ ?></th>
-                                <th class="align-middle text-center">
+                                <td class="align-middle text-center"><?= $no++ ?></td>
+                                <td class="align-middle text-center">
                                     <img src="<?= base_url() ?>assets/images/anggota/<?= $da['agt_img_url'] ?>" alt="" width="75" height="100">
-                                </th>
-                                <th class="align-middle text-center"><?= $da['agt_no_id'] ?></th>
-                                <th class="align-middle text-center"><?= $da['agt_nama'] ?></th>
-                                <th class="align-middle text-center"><?= $da['agt_dob'] ?></th>
-                                <th class="align-middle text-center"><?= $da['agt_alamat'] ?></th>
-                                <th class="align-middle text-center"><?= $da['agt_no_telp'] ?></th>
-                                <th class="align-middle text-center">
+                                </td>
+                                <td class="align-middle text-center"><?= $da['agt_no_id'] ?></td>
+                                <td class="align-middle text-center"><?= $da['agt_nama'] ?></td>
+                                <td class="align-middle text-center"><?= $da['agt_dob'] ?></td>
+                                <td class="align-middle text-center"><?= $da['agt_alamat'] ?></td>
+                                <td class="align-middle text-center"><?= $da['agt_no_telp'] ?></td>
+                                <td class="align-middle text-center">
                                     <!--Hapus-->
                                     <a href="#" class="btn btn-sm btn-warning"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                                             <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
@@ -84,7 +84,7 @@
                                             <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
                                         </svg></a>
 
-                                </th>
+                                </td>
                             </tr>
                         <?php endforeach; ?>
                     </tbody>
